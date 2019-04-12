43d651da09c11e95647d1c66dfccf5b589e1f0b2
nikhilagr <nikhil.agrawal005@gmail.com>
2019-04-11 23:11:50 -0700

    Completed User Registration flow and added var in User model

--- Colab/Colab/Model/User.swift
+++ Colab/Colab/Model/User.swift
@@ -13,23 +13,39 @@ class User{
    
     private(set) var user_id: String!
     private(set) var user_auth_id: String!
-    private(set) var firtst_name: String!
+    private(set) var first_name: String!
     private(set) var last_name: String!
     private(set) var profile_url: String!
     private(set) var email: String!
     private(set) var dob: String!
+    
+    var dictionary: [String: Any] {
+        
+        return [
+            "user_id":user_id,
+            "user_auth_id":user_auth_id,
+            "first_name":first_name,
+            "last_name":last_name,
+            "profile_url":profile_url,
+            "email":email,
+            "dob":dob
+        ]
+        
+    }
     
     
     init(userId: String, userauthId: String, firstName: String, lastName: String, profileUrl: String, email: String, dob: String) {
         
         self.user_id = userId
         self.user_auth_id = userauthId
-        self.firtst_name = firstName
+        self.first_name = firstName
         self.last_name = lastName
         self.profile_url = profileUrl
         self.email = email
         self.dob = dob
     }
+    
+
     
     
 }
--- Colab/Colab/ViewController.swift
+++ Colab/Colab/ViewController.swift
@@ -7,13 +7,65 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
+
+
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("View Did Load")
+        
+        registerNewUser(username: "nikhil.agrawal@sjsu.edu", password:"123456")
+        
     }
+    
+    
+    
+    func registerNewUser(username: String,password: String){
+        Auth.auth().createUser(withEmail: username, password: password) { (authDataResult, error) in
+            if let err = error{
+                //error occured
+                debugPrint(err.localizedDescription)
+            }else{
+                //TODO: Send verification email to user
+                // Add Details of users in users collection
+                
+                authDataResult?.user.sendEmailVerification(completion: { (error) in
+                    
+                    if let err = error{
+                        debugPrint(err.localizedDescription)
+                        print("Failed to send verification email")
+                    }else{
+                        
+                        print("please check verification email")
+                        let db = Firestore.firestore()
+                        var ref = db.collection("users").document()
+                        let newUser = User(userId: ref.documentID, userauthId: (Auth.auth().currentUser?.uid)!,firstName:"", lastName: "", profileUrl: "", email: username, dob: "")
+                        
+                        ref.setData(newUser.dictionary, completion: { (error) in
+                            
+                           if let err = error{
+                                debugPrint(err.localizedDescription)
+                           }else{
+                            print("successfully added user to db")
+                            }
+                        })
+                    }
+                })
+                print("Success")
+            }
+        
+        }
+        
+    }
+
 
 
 }
